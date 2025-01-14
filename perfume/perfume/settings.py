1028a288aadab6f0f9d441770844478b40880bd1
ittirarichan <abhishekbinish86@gmail.com>
2025-01-14 16:15:28 +0530

    payment gateway ongoing

--- perfume/perfume/settings.py
+++ perfume/perfume/settings.py
@@ -130,5 +130,3 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-RAZORPAY_KEY_ID = 'rzp_test_NHSwGMZJlC3tjS'
-RAZORPAY_KEY_SECRET = 'qxR8BKBiItXb5cX8cZMIl3uO'
